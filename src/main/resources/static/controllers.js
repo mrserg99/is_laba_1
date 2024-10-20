3f8afc68586406ca419bdf5cc17e5f913d5f3dd6
coffinbean <evgeniazabirova07@gmail.com>
2024-10-20 15:07:58 +0300

    кнопка регистрации добавлена

--- src/main/resources/static/controllers.js
+++ src/main/resources/static/controllers.js
@@ -40,3 +40,53 @@ function del_stud(){
 function del_group(){
     /*Удаляет объект группу*/
 }
+
+function add_stroks(){
+
+    document.getElementById("table_wrapper1").innerHTML = "<tr>\n" +
+        "        <td class=\"var_tab\"  id=\"2_std_id\">263879</td>\n" +
+        "        <td class=\"var_tab\" >P3311</td>\n" +
+        "        <td class=\"var_tab\" >22</td>\n" +
+        "        <td class=\"var_tab\" >22</td>\n" +
+        "        <td class=\"var_tab\" >22</td>\n" +
+        "        <td class=\"var_tab\" >2</td>\n" +
+        "        <td class=\"var_tab\" >2</td>\n" +
+        "        <td class=\"var_tab\" >12</td>\n" +
+        "        <td class=\"var_tab\" >19/09/2024 12:24:11</td>\n" +
+        "        <td class=\"var_tab\" >очная</td>\n" +
+        "        <td class=\"var_tab\" >KOT3CHKa</td>\n" +
+        "        <td class=\"var_tab\" >4.5</td>\n" +
+        "        <td class=\"var_tab\" >Лебедева А.И.</td>\n" +
+        "        <td class=\"var_tab\" >6</td>\n" +
+        "        <td class=\"var_tab\" ><button class=\"change button_dark\" onclick=\"change(2)\">Изменить</button></td>\n" +
+        "        <td class=\"var_tab\" >\n" +
+        "            <button class=\"del button_dark popup_del_btn\">\n" +
+        "                <img class=\"rubbish\" src=\"./pics/Mask%20group.png\" alt=\"\">\n" +
+        "            </button>\n" +
+        "        </td>\n" +
+        "    </tr>"
+    document.getElementById("table_wrapper2").innerHTML = "<tr>\n" +
+        "        <td class=\"var_tab\"  id=\"2_std_id\">263879</td>\n" +
+        "        <td class=\"var_tab\" >P3311</td>\n" +
+        "        <td class=\"var_tab\" >22</td>\n" +
+        "        <td class=\"var_tab\" >22</td>\n" +
+        "        <td class=\"var_tab\" >22</td>\n" +
+        "        <td class=\"var_tab\" >2</td>\n" +
+        "        <td class=\"var_tab\" >2</td>\n" +
+        "        <td class=\"var_tab\" >12</td>\n" +
+        "        <td class=\"var_tab\" >19/09/2024 12:24:11</td>\n" +
+        "        <td class=\"var_tab\" >очная</td>\n" +
+        "        <td class=\"var_tab\" >KOT3CHKa</td>\n" +
+        "        <td class=\"var_tab\" >4.5</td>\n" +
+        "        <td class=\"var_tab\" >Лебедева А.И.</td>\n" +
+        "        <td class=\"var_tab\" >6</td>\n" +
+        "        <td class=\"var_tab\" ><button class=\"change button_dark\" onclick=\"change(2)\">Изменить</button></td>\n" +
+        "        <td class=\"var_tab\" >\n" +
+        "            <button class=\"del button_dark popup_del_btn\">\n" +
+        "                <img class=\"rubbish\" src=\"./pics/Mask%20group.png\" alt=\"\">\n" +
+        "            </button>\n" +
+        "        </td>\n" +
+        "    </tr>"
+
+
+}
